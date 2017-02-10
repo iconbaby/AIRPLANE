554010f8faa029ef72d65115a9b5af8d5b85970c
slkk <644187060@qq.com>
2017-02-10 23:10:39 +0800

    complete the ui

---
+++ app/src/main/java/com/slkk/airplane/homepage/BookMarksFragment.java
@@ -0,0 +1,26 @@
+package com.slkk.airplane.homepage;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by skll on 2017/2/10.
+ */
+
+public class BookMarksFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
--- app/src/main/java/com/slkk/airplane/homepage/MainActivity.java
+++ app/src/main/java/com/slkk/airplane/homepage/MainActivity.java
@@ -1,15 +1,94 @@
 package com.slkk.airplane.homepage;
 
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.slkk.airplane.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private MainFragment mainFragment;
+    private BookMarksFragment bookMarksFragment;
+    private final String ACTION_BOOKMARKS = "com.slkk.homepage.BookMarksFragment";
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+
+        if (!mainFragment.isAdded()) {
+            getSupportFragmentManager().beginTransaction().add(R.id.layout_fragment, mainFragment).commit();
+        }
+        if (!bookMarksFragment.isAdded()) {
+            getSupportFragmentManager().beginTransaction().add(R.id.layout_fragment, bookMarksFragment).commit();
+        }
+
+        String action = getIntent().getAction();
+        if (action.equals(ACTION_BOOKMARKS)) {
+            showBookmarksFragment();
+        } else {
+            showMainFragment();
+        }
+    }
+
+    private void showMainFragment() {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.show(mainFragment);
+        fragmentTransaction.hide(bookMarksFragment);
+        fragmentTransaction.commit();
+        toolbar.setTitle(R.string.app_name);
+
+    }
+
+    private void showBookmarksFragment() {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.show(bookMarksFragment);
+        fragmentTransaction.hide(mainFragment);
+        fragmentTransaction.commit();
+        toolbar.setTitle(R.string.book_marks);
+
+    }
+
+    private void initView() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.open_drawer_toggle,
+                R.string.close_drawer_toggle);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        switch (itemId) {
+            case R.id.nav_home:
+                showMainFragment();
+                break;
+            case R.id.nav_bookmarks:
+                showBookmarksFragment();
+                break;
+            case R.id.nav_change_theme:
+                break;
+            case R.id.nav_settings:
+                break;
+
+        }
+
+        return false;
     }
 }
