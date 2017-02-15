6e70ab2fe53e08aa1f0440ba3ebe36f9acecd96a
enw <644187060@qq.com>
2017-02-15 19:02:19 +0800

    tiji

--- app/src/main/java/com/slkk/airplane/adapter/ZhihuDailyNewsAdapter.java
+++ app/src/main/java/com/slkk/airplane/adapter/ZhihuDailyNewsAdapter.java
@@ -1,18 +1,41 @@
 package com.slkk.airplane.adapter;
 
+import android.content.Context;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
+import com.slkk.airplane.bean.ZhihuDailyNews;
 import com.slkk.airplane.interfaze.OnRecyclerViewOnClickListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by dell on 2017/2/15.
  */
 
 public class ZhihuDailyNewsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private final Context context;
+    private final LayoutInflater inflater;
+    private List<ZhihuDailyNews.Question> list = new ArrayList<ZhihuDailyNews.Question>();
+
+    private static final int TYPE_NORMAL = 0;
+    private static final int TYPE_FOOTER =1;
+    public ZhihuDailyNewsAdapter(Context context, List<ZhihuDailyNews.Question> list) {
+        this.context = context;
+        this.list = list;
+        this.inflater = LayoutInflater.from(context);
+    }
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        switch (viewType) {
+            case TYPE_NORMAL:
+                return new Nor
+        }
         return null;
     }
 
@@ -28,5 +51,19 @@ public class ZhihuDailyNewsAdapter extends RecyclerView.Adapter<RecyclerView.Vie
 
     public void setItemClickListener(OnRecyclerViewOnClickListener listener) {
 
+    }
+
+    public class NormalViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        public NormalViewHolder(View itemView, OnRecyclerViewOnClickListener listener) {
+            itemView.findViewById(R.id.)
+            super(itemView);
+
+        }
+
+        @Override
+        public void onClick(View view) {
+
+        }
     }
 }
